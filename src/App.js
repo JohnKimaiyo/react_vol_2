abf4ed3706cb1db5ebfbad2db85c574f8c3d25e5
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-08 08:05:22 +0300

    count usestate

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Hello from "./components/Hello";
 import Title from "./components/Title";
 import MyName from "./components/MyName";
 import Form from "./components/Form";
-
+import Mycount from "./components/Mycount";
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
       <Title name="Solidity Dev Kimaiyo" />
       <MyName/>
       <Form/>
+      <Mycount/>
     </div>
   );
 }
