23641b26b10839a6a0519c50bd26218b5a39ac5d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-14 10:03:19 -0800

    booklist component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React from "react";
 import "./App.css";
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
-import Title from "./components/Title";
-import MyName from "./components/MyName";
-import Form from "./components/Form";
-import Mycount from "./components/Mycount";
-import Users from "./components/Users";
-import Event from "./components/Event";
-import Range from "./components/Range";
-import Submit from "./components/Submit";
-import InputFocus from "./components/inputFocus";
+
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
-      <Hello />
-      <Title name="Frontend Dev Kimaiyo" />
-      <Title name="Mobile Dev Kimaiyo" />
-      <Title name="Solidity Dev Kimaiyo" />
-      <MyName/>
-      <Form/>
-      <Mycount/>
-      <Users/>
-      <Event/>
-      <Range/>
-      <Submit/>
-      <InputFocus/>
+      
     </div>
   );
 }
